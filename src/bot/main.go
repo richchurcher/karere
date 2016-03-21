612e290f67081eb90683f8c4d50de9680c56e995
Rich Churcher <rich.churcher@gmail.com>
2016-03-22 00:49:11 +1300

    Make a start on OAuth2 flow

--- src/bot/main.go
+++ src/bot/main.go
@@ -7,8 +7,10 @@ import (
 	"time"
 
 	slackbot "github.com/BeepBoopHQ/go-slackbot"
+	"github.com/google/go-github/github"
 	"github.com/nlopes/slack"
 	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
 )
 
 const (
@@ -16,28 +18,38 @@ const (
 	WithoutTyping = slackbot.WithoutTyping
 
 	HelpText = "Karere uses git-style command syntax:\n" +
-		" \u2022 `about`: access _all_, implemented _yes_.\n" +
-		" \u2022 `add <github-user> <slack-user> [cohort-repo]`: access _restrict_, implemented _no_.\n" +
-		" \u2022 `blocks [cohort-repo]`: access _restrict_, implemented _no_.\n" +
-		" \u2022 `gist [add|rm] [gist-name]`: access _all_, implemented _no_.\n" +
-		" \u2022 `help`: access _all_, implemented _partial_.\n" +
-		" \u2022 `init [cohort-name]`: access _restrict_, implemented _no_.\n" +
-		" \u2022 `log [cohort-name]`: access _restrict_, implemented _no_.\n" +
-		" \u2022 `mv <github-user> <old-cohort-repo> <new-cohort-repo>: access _restrict_, implemented _no_.`\n" +
-		" \u2022 `push u2w3 [cohort-repo]`: access _restrict_, implemented _no_.\n" +
-		" \u2022 `reset u1w1 [cohort-repo]`: access _restrict_, implemented _no_.\n" +
-		" \u2022 `rm <github-user> [cohort-repo]`: access _restrict_, implemented _no_.\n" +
-		" \u2022 `snip [add|rm] [snippet-name]`: access _all_, implemented _no_.\n" +
-		" \u2022 `version`: access _restrict_, implemented _no_.\n" +
+		"\u2022 `about`: access _all_, implemented _yes_.\n" +
+		"\u2022 `add <github-user> <slack-user> [cohort-repo]`: access _restrict_, implemented _no_.\n" +
+		"\u2022 `blocks [cohort-repo]`: access _restrict_, implemented _no_.\n" +
+		"\u2022 `gist [add|rm] [gist-name]`: access _all_, implemented _no_.\n" +
+		"\u2022 `help`: access _all_, implemented _partial_.\n" +
+		"\u2022 `init [cohort-name]`: access _restrict_, implemented _no_.\n" +
+		"\u2022 `log [cohort-name]`: access _restrict_, implemented _no_.\n" +
+		"\u2022 `mv <github-user> <old-cohort-repo> <new-cohort-repo>: access _restrict_, implemented _no_.`\n" +
+		"\u2022 `push u2w3 [cohort-repo]`: access _restrict_, implemented _no_.\n" +
+		"\u2022 `reset u1w1 [cohort-repo]`: access _restrict_, implemented _no_.\n" +
+		"\u2022 `rm <github-user> [cohort-repo]`: access _restrict_, implemented _no_.\n" +
+		"\u2022 `snip [add|rm] [snippet-name]`: access _all_, implemented _no_.\n" +
+		"\u2022 `version`: access _restrict_, implemented _no_.\n" +
 		"In all cases where _cohort-repo_ is omitted, Karere will attempt to use the Slack channel name instead."
+
+	token = os.Getenv("GITHUB_ACCESS_TOKEN")
 )
 
 var greetingPrefixes = []string{"Hi", "Hello", "Hey", "Kia ora"}
 
 func main() {
 	bot := slackbot.New(os.Getenv("SLACK_TOKEN"))
+	client := github.NewClient(nil)
 
 	toMe := bot.Messages(slackbot.DirectMessage, slackbot.DirectMention).Subrouter()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	client := github.NewClient(tc)
+	// list all repositories for the authenticated user
+	//repos, _, err := client.Repositories.List("", nil)
 
 	hi := "hi|hello|hey|kia ora|tena koe"
 	toMe.Hear(hi).MessageHandler(HelloHandler)
